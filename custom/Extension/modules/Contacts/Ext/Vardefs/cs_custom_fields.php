c207c4c0cf917a303b5eddba200949862c4fed96
LluisSerrano <136106881+LluisSerrano@users.noreply.github.com>
2026-04-08 17:16:30 +0200

    Create cs_custom_fields.php

---
+++ custom/Extension/modules/Contacts/Ext/Vardefs/cs_custom_fields.php
@@ -0,0 +1,53 @@
+<?php
+$dictionary['Contacts']['fields']['stic_cs_h_majors_c'] = [
+    'name' => 'stic_cs_h_majors_c',
+    'vname' => 'LBL_STIC_CS_H_MAJORS',
+    'type' => 'int',
+    'dbType' => 'int',
+    'len' => '11',
+    'validation' => [
+        'type' => 'range',
+        'min' => 0,
+    ],
+    'audited' => false,
+    'importable' => 'true',
+];
+$dictionary['stic_Sessions']['fields']['stic_cs_d_majors_c'] = [
+    'name' => 'stic_cs_d_majors_c',
+    'vname' => 'LBL_STIC_CS_D_MAJORS',
+    'type' => 'int',
+    'dbType' => 'int',
+    'len' => '11',
+    'validation' => [
+        'type' => 'range',
+        'min' => 0,
+    ],
+    'audited' => false,
+    'importable' => 'true',
+];
+$dictionary['stic_Sessions']['fields']['stic_cs_h_minors_c'] = [
+    'name' => 'stic_cs_h_minors_c',
+    'vname' => 'LBL_STIC_CS_H_MINORS',
+    'type' => 'int',
+    'dbType' => 'int',
+    'len' => '11',
+    'validation' => [
+        'type' => 'range',
+        'min' => 0,
+    ],
+    'audited' => false,
+    'importable' => 'true',
+];
+$dictionary['stic_Sessions']['fields']['stic_cs_d_minors_c'] = [
+    'name' => 'stic_cs_d_minors_c',
+    'vname' => 'LBL_STIC_CS_D_MINORS',
+    'type' => 'int',
+    'dbType' => 'int',
+    'len' => '11',
+    'validation' => [
+        'type' => 'range',
+        'min' => 0,
+    ],
+    'audited' => false,
+    'importable' => 'true',
+];
