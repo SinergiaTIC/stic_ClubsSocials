b09e178a235c81a5550095a751d49e08c2d18069
LluisSerrano <136106881+LluisSerrano@users.noreply.github.com>
2026-04-08 17:14:28 +0200

    Create ca_ES.add_relationship_type.php

---
+++ custom/Extension/application/Ext/Language/ca_ES.add_relationship_type.php
@@ -0,0 +1,9 @@
+<?php
+
+$app_list_strings['stic_contacts_relationships_types_list']['pre_user'] = 'Preusuari de serveis';
+$app_list_strings['stic_contacts_relationships_end_reasons_list']['exitus'] = 'Exitus';
+$app_list_strings['stic_contacts_relationships_end_reasons_list']['forced'] = 'Forçosa';
+$app_list_strings['stic_contacts_relationships_end_reasons_list']['medical'] = 'Mèdica';
+$app_list_strings['stic_contacts_relationships_end_reasons_list']['remotion'] = 'Remoció';
+$app_list_strings['stic_contacts_relationships_end_reasons_list']['judicial'] = 'Sent. Judicial';
+$app_list_strings['stic_contacts_relationships_end_reasons_list']['reserva'] = 'Reserva';
